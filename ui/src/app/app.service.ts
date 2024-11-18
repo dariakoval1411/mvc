d8bcf0f7d8b7b2f5d6a259b556e6936a5fb7c473
dariakoval1411 <dashakoval1411@gmail.com>
2024-11-18 19:51:15 +0100

    commit

--- ui/src/app/app.service.ts
+++ ui/src/app/app.service.ts
@@ -12,23 +12,23 @@ export class AppService {
     
     //Calculte 
     public calculateSum(num1: number, num2: number): Observable<number> { 
-        return this.httpClient.get<number>(`api-ui/api/calculate?number1=${num1}&number2=${num2}`);
+        return this.httpClient.get<number>(`api/calculate?number1=${num1}&number2=${num2}`);
     }
 
     //ToDoList
     public getToDoList(): Observable<ToDoItem[]> { 
-        return this.httpClient.get<ToDoItem[]>(`api-ui/api/todo`);
+        return this.httpClient.get<ToDoItem[]>(`api/todo`);
     }
 
     public addToDo(task: ToDoItem): Observable<ToDoItem> { 
-        return this.httpClient.post<ToDoItem>(`api-ui/api/todo`, task);
+        return this.httpClient.post<ToDoItem>(`api/todo`, task);
     }
 
     public deleteToDoItem(id: number): Observable<void>{
-        return this.httpClient.delete<void>(`api-ui/api/todo/${id}`);
+        return this.httpClient.delete<void>(`api/todo/${id}`);
     }
 
     public updateToDoItem(id: number,task: ToDoItem): Observable<ToDoItem>{
-        return this.httpClient.put<ToDoItem>(`api-ui/api/todo/${id}`, task);
+        return this.httpClient.put<ToDoItem>(`api/todo/${id}`, task);
     }
 }
